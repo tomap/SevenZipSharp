5bb9910d04be196c3f93a9063f79b8ee0a76ba92
Joel Ahlgren <joel@ahlgren.io>
2018-07-30 23:59:50 +0200

    Updated Async extractor tests.

--- SevenZip.Tests/SevenZipExtractorAsynchronousTests.cs
+++ SevenZip.Tests/SevenZipExtractorAsynchronousTests.cs
@@ -11,20 +11,26 @@
         [Test]
         public void AsynchronousExtractArchiveEventsTest()
         {
-            var extractingEventInvoked = false;
-            var extractionFinishedEventInvoked = false;
+            var extractingInvoked = 0;
+            var extractionFinishedInvoked = 0;
+            var fileExistsInvoked = 0;
+            var fileExtractionStartedInvoked = 0;
+            var fileExtractionFinishedInvoked = 0;
 
             using (var extractor = new SevenZipExtractor(@"TestData\multiple_files.7z"))
             {
                 extractor.EventSynchronization = EventSynchronizationStrategy.AlwaysSynchronous;
 
-                extractor.Extracting += (o, e) => extractingEventInvoked = true;
-                extractor.ExtractionFinished += (o, e) => extractionFinishedEventInvoked = true;
+                extractor.Extracting += (o, e) => extractingInvoked++;
+                extractor.ExtractionFinished += (o, e) => extractionFinishedInvoked++;
+                extractor.FileExists += (o, e) => fileExistsInvoked++;
+                extractor.FileExtractionStarted += (o, e) => fileExtractionStartedInvoked++;
+                extractor.FileExtractionFinished += (o, e) => fileExtractionFinishedInvoked++;
 
                 extractor.BeginExtractArchive(OutputDirectory);
 
-                var timeToWait = 250;
-                while (!extractionFinishedEventInvoked)
+                var timeToWait = 1000;
+                while (extractionFinishedInvoked == 0)
                 {
                     if (timeToWait <= 0)
                     {
@@ -34,35 +40,49 @@
                     Thread.Sleep(25);
                     timeToWait -= 25;
                 }
-            }
 
-            Assert.IsTrue(extractingEventInvoked);
-            Assert.IsTrue(extractionFinishedEventInvoked);
+                Assert.AreEqual(3, extractingInvoked);
+                Assert.AreEqual(1, extractionFinishedInvoked);
+                Assert.AreEqual(0, fileExistsInvoked);
+                Assert.AreEqual(3, fileExtractionStartedInvoked);
+                Assert.AreEqual(3, fileExtractionFinishedInvoked);
+
+                extractionFinishedInvoked = 0;
+                extractor.BeginExtractArchive(OutputDirectory);
+
+                timeToWait = 1000;
+                while (extractionFinishedInvoked == 0)
+                {
+                    if (timeToWait <= 0)
+                    {
+                        break;
+                    }
+
+                    Thread.Sleep(25);
+                    timeToWait -= 25;
+                }
+
+                Assert.AreEqual(3, fileExistsInvoked);
+            }
         }
 
         [Test]
         public void AsynchronousExtractFileEventsTest()
         {
-            var extractingEventInvoked = false;
-            var extractionFinishedEventInvoked = false;
-            var fileExtractionStartedInvoked = false;
-            var fileExtractionFinishedInvoked = false;
+            var extractionFinishedInvoked = false;
 
             var fileStream = File.Create(TemporaryFile);
 
             using (var extractor = new SevenZipExtractor(@"TestData\multiple_files.7z"))
             {
                 extractor.EventSynchronization = EventSynchronizationStrategy.AlwaysSynchronous;
-
-                extractor.FileExtractionStarted += (o, e) => fileExtractionStartedInvoked = true;
-                extractor.FileExtractionFinished += (o, e) => fileExtractionFinishedInvoked = true;
-                extractor.Extracting += (o, e) => extractingEventInvoked = true;
-                extractor.ExtractionFinished += (o, e) => extractionFinishedEventInvoked = true;
+                
+                extractor.ExtractionFinished += (o, e) => extractionFinishedInvoked = true;
 
                 extractor.BeginExtractFile(0, fileStream);
 
                 var timeToWait = 250;
-                while (!extractionFinishedEventInvoked)
+                while (!extractionFinishedInvoked)
                 {
                     if (timeToWait <= 0)
                     {
@@ -75,36 +95,25 @@
 
                 fileStream.Dispose();
             }
-
-            Assert.IsTrue(extractingEventInvoked);
-            Assert.IsTrue(extractionFinishedEventInvoked);
-            Assert.IsTrue(fileExtractionStartedInvoked);
-            Assert.IsTrue(fileExtractionFinishedInvoked);
-
+            
             Assert.AreEqual("file1", File.ReadAllText(TemporaryFile));
         }
 
         [Test]
         public void AsynchronousExtractFilesEventsTest()
         {
-            var extractingEventInvoked = false;
-            var extractionFinishedEventInvoked = false;
-            var fileExtractionStartedInvoked = false;
-            var fileExtractionFinishedInvoked = false;
+            var extractionFinishedInvoked = false;
 
             using (var extractor = new SevenZipExtractor(@"TestData\multiple_files.7z"))
             {
                 extractor.EventSynchronization = EventSynchronizationStrategy.AlwaysSynchronous;
-
-                extractor.FileExtractionStarted += (o, e) => fileExtractionStartedInvoked = true;
-                extractor.FileExtractionFinished += (o, e) => fileExtractionFinishedInvoked = true;
-                extractor.Extracting += (o, e) => extractingEventInvoked = true;
-                extractor.ExtractionFinished += (o, e) => extractionFinishedEventInvoked = true;
+                
+                extractor.ExtractionFinished += (o, e) => extractionFinishedInvoked = true;
 
                 extractor.BeginExtractFiles(OutputDirectory, new [] { 0, 2 });
 
                 var timeToWait = 250;
-                while (!extractionFinishedEventInvoked)
+                while (!extractionFinishedInvoked)
                 {
                     if (timeToWait <= 0)
                     {
@@ -115,11 +124,6 @@
                     timeToWait -= 25;
                 }
             }
-
-            Assert.IsTrue(extractingEventInvoked);
-            Assert.IsTrue(extractionFinishedEventInvoked);
-            Assert.IsTrue(fileExtractionStartedInvoked);
-            Assert.IsTrue(fileExtractionFinishedInvoked);
 
             Assert.AreEqual(2, Directory.GetFiles(OutputDirectory).Length);
         }
